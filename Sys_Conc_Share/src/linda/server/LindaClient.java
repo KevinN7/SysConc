8fcde0e3f7bd3396f4a88b7d651c72bfad070ebd
gabriel <gabriel@172.21.210.222>
2015-01-14 10:00:38 +0100

    creation de LindaServer, ecriture de LindaClient

--- Sys_Conc_Share/src/linda/server/LindaClient.java
+++ Sys_Conc_Share/src/linda/server/LindaClient.java
@@ -1,76 +1,77 @@
 package linda.server;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.Collection;
 
 import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
+import linda.TupleFormatException;
 
 /** Client part of a client/server implementation of Linda.
  * It implements the Linda interface and propagates everything to the server it is connected to.
  * */
 public class LindaClient implements Linda {
 	
+	LindaServer serv;
+	
     /** Initializes the Linda implementation.
      *  @param serverURI the URI of the server, e.g. "//localhost:4000/LindaServer".
+     * @throws NotBoundException 
+     * @throws RemoteException 
+     * @throws MalformedURLException 
      */
-    public LindaClient(String serverURI) {
-        // TO BE COMPLETED
+    public LindaClient(String serverURI) throws MalformedURLException, RemoteException, NotBoundException {
+        serv = (LindaServer)Naming.lookup(serverURI );
     }
 
 	@Override
 	public void write(Tuple t) {
-		// TODO Auto-generated method stub
-		
+		serv.write(t);		
 	}
 
 	@Override
 	public Tuple take(Tuple template) {
-		// TODO Auto-generated method stub
-		return null;
+		return serv.take(template);
 	}
 
 	@Override
 	public Tuple read(Tuple template) {
-		// TODO Auto-generated method stub
-		return null;
+		return serv.read(template);
 	}
 
 	@Override
 	public Tuple tryTake(Tuple template) {
-		// TODO Auto-generated method stub
-		return null;
+		return serv.tryTake(template);
 	}
 
 	@Override
 	public Tuple tryRead(Tuple template) {
-		// TODO Auto-generated method stub
-		return null;
+		return serv.tryRead(template);
 	}
 
 	@Override
 	public Collection<Tuple> takeAll(Tuple template) {
-		// TODO Auto-generated method stub
-		return null;
+		return serv.takeAll(template);
 	}
 
 	@Override
 	public Collection<Tuple> readAll(Tuple template) {
-		// TODO Auto-generated method stub
-		return null;
+		return serv.readAll(template);
 	}
 
 	@Override
 	public void eventRegister(eventMode mode, eventTiming timing,
 			Tuple template, Callback callback) {
-		// TODO Auto-generated method stub
-		
+		serv.eventRegister(mode,timing,template,callback);
 	}
 
 	@Override
 	public void debug(String prefix) {
-		// TODO Auto-generated method stub
-		
+		serv.debug(prefix);		
 	}
     
     // TO BE COMPLETED
---
+++ Sys_Conc_Share/src/linda/server/LindaServer.java
@@ -0,0 +1,83 @@
+package linda.server;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Collection;
+
+import linda.Callback;
+import linda.Linda;
+import linda.Tuple;
+import linda.shm.CentralizedLinda;
+
+public class LindaServer extends UnicastRemoteObject implements Linda {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	CentralizedLinda linda;
+	Registry registry;
+
+	protected LindaServer(String uri, int port) throws RemoteException {
+		registry = LocateRegistry.createRegistry(port);
+		linda = new CentralizedLinda();
+		try {
+			Naming.rebind(uri, this);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void write(Tuple t) {
+		linda.write(t);
+	}
+
+	@Override
+	public Tuple take(Tuple template) {
+		return linda.take(template);
+	}
+
+	@Override
+	public Tuple read(Tuple template) {
+		return linda.read(template);
+	}
+
+	@Override
+	public Tuple tryTake(Tuple template) {
+		return tryTake(template);
+	}
+
+	@Override
+	public Tuple tryRead(Tuple template) {
+		return linda.tryRead(template);
+	}
+
+	@Override
+	public Collection<Tuple> takeAll(Tuple template) {
+		return linda.takeAll(template);
+	}
+
+	@Override
+	public Collection<Tuple> readAll(Tuple template) {
+		return linda.readAll(template);
+	}
+
+	@Override
+	public void eventRegister(eventMode mode, eventTiming timing,
+			Tuple template, Callback callback) {
+		linda.eventRegister(mode,timing,template,callback);
+	}
+
+	@Override
+	public void debug(String prefix) {
+		linda.debug(prefix);
+	}
+
+}
